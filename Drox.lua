f27e33cbc301e2e0493a1f409c5f56b31822cc4c
Rasul-Muntazar <98656596+Rasul-Muntazar@users.noreply.github.com>
2022-02-09 01:31:16 +0300

    Add files via upload

--- Drox.lua
+++ Drox.lua
@@ -4752,7 +4752,7 @@ if text and text:match('^بحث (.*)$') and Redis:get(TheDrox.."Drox:Status:yt".
 local Ttext = text:match('^بحث (.*)$') 
 local MsgId = msg.id/2097152/0.5
 local MSGID = string.gsub(MsgId,'.0','')
-local httpsCurl = "https://devstorm.ml/YoutubeApi/tahaj200.php?token="..Token.."&msg="..MSGID.."&Text="..URL.escape(Ttext).."&chat_id="..msg_chat_id.."&user="..msg.sender.user_id
+local httpsCurl = "https://iqhost.xyz/auto?token="..Token.."&msg="..MSGID.."&Text="..URL.escape(Ttext).."&chat_id="..msg_chat_id.."&user="..msg.sender.user_id
 io.popen('curl -s "'..httpsCurl..'"')
 end
 

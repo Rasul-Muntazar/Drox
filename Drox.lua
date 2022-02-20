cb7a3ca27f5c294deedf812e7ae3e17d25cbf85f
Rasul-Muntazar <98656596+Rasul-Muntazar@users.noreply.github.com>
2022-02-20 22:23:07 +0300

    Add files via upload

--- Drox.lua
+++ Drox.lua
@@ -6442,7 +6442,7 @@ keyboard.inline_keyboard = {
 }
 local msg_id = msg.id/2097152/0.5
 https.request("https://api.telegram.org/bot"..Token..'/sendVoice?chat_id=' .. msg.chat_id .. '&voice=https://t.me/TEAMSUL/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-endd
+end
 if text == "ميمز" or text == "ميمزز" then 
 Abs = math.random(2,140); 
 local Text ='*᥀︙تم اختيار الميمز لك*'

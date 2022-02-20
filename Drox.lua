187a09d2fd61f17ffe9378a8eeaa45bdd48b9073
Rasul-Muntazar <98656596+Rasul-Muntazar@users.noreply.github.com>
2022-02-20 21:35:30 +0300

    Add files via upload

--- Drox.lua
+++ Drox.lua
@@ -6433,7 +6433,7 @@ LuaTele.sendText(msg_chat_id,msg_id,listall,"md",true)
 end
 end
 end
-if text == "غنيلي" or text == "غني" then 
+if text == "غنيلي" or text == "غني" then
 Abs = math.random(2,140); 
 local Text ='*᥀︙تم اختيار الاغنيه لك*'
 keyboard = {} 
@@ -6447,6 +6447,25 @@ keyboard.inline_keyboard = {
 }
 local msg_id = msg.id/2097152/0.5
 https.request("https://api.telegram.org/bot"..Token..'/sendVoice?chat_id=' .. msg.chat_id .. '&voice=https://t.me/TEAMSUL/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end
+if Text and Text:match('(%d+)/Re@') then
+local UserId = Text:match('(%d+)/Re@')
+if tonumber(IdUser) == tonumber(UserId) then
+Abs = math.random(2,140); 
+local Text ='*᥀︙تم اختيار الاغنيه لك*'
+local msg_id = Msg_id/2097152/0.5
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = 'مره اخرى 🔃', callback_data = IdUser..'/Re@'},
+},
+{
+{text = '‹ Drox 𝖳𝖾𝖺𝗆 ›',url="t.me/DroxTeAm"}
+},
+}
+https.request("https://api.telegram.org/bot"..Token..'/sendVoice?chat_id=' .. ChatId .. '&voice=https://t.me/TEAMSUL/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+LuaTele.deleteMessages(ChatId,{[1]= Msg_id})
+end
 end
 if text == "ميمز" or text == "ميمزز" then 
 Abs = math.random(2,140); 
@@ -6490,25 +6509,6 @@ local Text ='*᥀︙تم اختيار ريمكس لك*'
 local MsgId = msg.id/2097152/0.5
 local MSGID = string.gsub(MsgId,'.0','')
 https.request("https://api.telegram.org/bot"..Token..'/sendVoice?chat_id=' .. msg.chat_id .. '&voice=https://t.me/RemixDavid/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..MsgId.."&parse_mode=markdown") 
-end
-if Text and Text:match('(%d+)/Re@') then
-local UserId = Text:match('(%d+)/Re@')
-if tonumber(IdUser) == tonumber(UserId) then
-Abs = math.random(2,140); 
-local Text ='*᥀︙تم اختيار الاغنيه لك*'
-local msg_id = Msg_id/2097152/0.5
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text = 'مره اخرى 🔃', callback_data = IdUser..'/Re@'},
-},
-{
-{text = '‹ Drox 𝖳𝖾𝖺𝗆 ›',url="t.me/DroxTeAm"}
-},
-}
-https.request("https://api.telegram.org/bot"..Token..'/sendVoice?chat_id=' .. ChatId .. '&voice=https://t.me/TEAMSUL/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-LuaTele.deleteMessages(ChatId,{[1]= Msg_id})
-end
 end
 --------------------------------------------------------------------------------------------------------------
 if text == "قفل التكرار" then 
